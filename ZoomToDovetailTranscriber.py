d0f3563e92af79ce8fddbd7c7b09357a68b42f96
Andrew Nelson <andy@andyhub.com>
2021-08-25 13:21:24 -0400

    added the thing

---
+++ ZoomToDovetailTranscriber.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+import re
+import sys
+
+infile = open(sys.argv[1], 'r')
+
+def replacer(matchobj):
+    name = matchobj.group(0).replace(':','')
+    return '<v %s>' % name
+
+outcontent = ''
+for line in infile:
+  stripline = line.strip()
+  newline = re.sub(r'([a-zA-Z].*?:)', replacer, stripline)
+  outcontent += newline + '\n'
+infile.close()
+
+outfileName = sys.argv[1].replace(".vtt", "_fordovetail.vtt")
+outfile = open(outfileName, 'w')
+outfile.write(outcontent)
+outfile.close()
